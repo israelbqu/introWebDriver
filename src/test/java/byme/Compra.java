cb55259d1e890e43500d1b7114ba67e0486e8262
Israel Bibiano <israel.bibiano92@gmail.com>
2021-03-16 20:24:11 -0600

    Se agregan dos ejercicios de tarea

---
+++ src/test/java/byme/Compra.java
@@ -0,0 +1,61 @@
+package byme;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Compra {
+    static WebDriver driver;
+    public static void main(String[] args) {
+
+        driver = new ChromeDriver();
+        driver.get("http:vamonos-finance.herokuapp.com");
+        WebElement linkregistro = driver.findElement(By.cssSelector("[href='/register']"));
+        if(linkregistro.isDisplayed()){
+            System.out.println("Link de registro visible");
+        }
+        //campoUsername://css="[name="username"]"
+        //campoPassword://css="[name="password"]"
+        //buttonLogin://css=".btn"
+        //tablaCompras://css=".table"
+        //
+        //ligaQuote; // css = [href="/quote"]
+        //ligaBuy;// css = [href="/buy"]
+        //ligaSell;// css = [href="/sell"]
+        //ligaHistory;// css = [href="/history"]
+        //ligaLogOut;// css = [href="/logout"]
+
+        WebElement CampoUsername = driver.findElement(By.cssSelector("[name='username']")),
+                campoPassword = driver.findElement(By.cssSelector("[name='password']")),
+                buttonLogin = driver.findElement(By.cssSelector(".btn"));
+        CampoUsername.sendKeys("Juanito.Banana");
+        campoPassword.sendKeys("Testing1234");
+        buttonLogin.click();
+
+//        ligaBuy;// css = [href="/buy"]
+//        campoSymbol; //name="symbol"
+//        campoQuantity; //name="qty"
+//        botonBuy;//css=".btn"
+//        tablaCompras://css=".table"
+
+        WebElement LigaBuy = driver.findElement(By.cssSelector("[href='/buy']"));
+        LigaBuy.click();
+
+        WebElement campoSymbol = driver.findElement(By.name("symbol")),
+                campoQuatity = driver.findElement(By.name("qty")),
+                botonBuy = driver.findElement(By.cssSelector(".btn"));
+        campoSymbol.sendKeys("MSFT");
+        campoQuatity.sendKeys("1");
+        botonBuy.click();
+
+        WebElement tablaCompas = driver.findElement(By.cssSelector(".table"));
+        if(tablaCompas.isDisplayed()){
+            System.out.println("La prueba fue exitosa.");
+        }
+        else{
+            System.out.println("Valió mae");
+        }
+
+    }
+}
---
+++ src/test/java/byme/practiceone.java
@@ -0,0 +1,54 @@
+package byme;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class practiceone {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+//Abrir el navegador
+        driver = new ChromeDriver();
+        //Navegar a la página
+        //Se tiene la URL
+        driver.get("http:vamonos-finance.herokuapp.com");
+        //Verificar que muestra la pantalla de login
+        //Está link de registro?
+        WebElement linkregistro = driver.findElement(By.cssSelector("[href='/register']"));
+
+        if(linkregistro.isDisplayed()){
+            System.out.println("Link de registro visible");
+        }
+
+        //Registrar al usuario
+        //Click al botón de Registro.
+        linkregistro.click();
+        WebElement campoUsuario = driver.findElement(By.cssSelector("[name='username'")),
+        campoPassword = driver.findElement(By.cssSelector("[name='password']")),
+        campoRepeat = driver.findElement(By.cssSelector("[name='repeat_password']")),
+        button = driver.findElement(By.cssSelector(".btn"));
+
+        for (int i = 0; i < 10; i++) {
+            campoUsuario.sendKeys("Israel.Bibiano" + i);
+        }
+        campoPassword.sendKeys("prueba123");
+        campoRepeat.sendKeys("prueba1234");
+
+
+        if(campoPassword == campoRepeat){
+        button.click();
+            System.out.println("El registro se creó exitosamente");
+        }
+        else{
+            System.out.println("Prueba completada sin contraseñas iguales");
+        }
+
+
+
+
+
+
+    }
+}
